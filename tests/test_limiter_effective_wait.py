96179d0f8fce0b6175b2271510b1e1a7e62c2a94
Pierre Souchay <pierre@souchay.net>
2025-03-17 21:01:21 +0100

    Added test case that fails

--- tests/test_limiter_effective_wait.py
+++ tests/test_limiter_effective_wait.py
@@ -1,6 +1,6 @@
 
 import asyncio as asyncio
-from typing import Any, Awaitable
+from typing import Any, Awaitable, Union
 import inspect
 import time
 from asynciolimiter import LeakyBucketLimiter, Limiter, StrictLimiter, _CommonLimiterMixin
@@ -10,40 +10,63 @@ class FastFunc:
         self.calls = 0
         self.limiter = limiter
         assert limiter
-        self.queue: asyncio.Queue[Awaitable[Any]]
+        self.queue: asyncio.Queue[Awaitable[Any]] = asyncio.Queue()
     
     async def perform_quick_call(self) -> None:
         await asyncio.sleep(0.1)
-        await self.limiter.wait()
-        await asyncio.sleep(0.1)
         self.calls += 1
-        await asyncio.sleep(0.1)
 
+
+STOP_FETCHING = object()
+
+async def consume_updates(
+    rate_limiter:  _CommonLimiterMixin,
+    q: asyncio.Queue[Union[object, Awaitable[Any]]]
+) -> None:
+    to_await = await q.get()
+    while to_await is not STOP_FETCHING:
+        assert isinstance(to_await, Awaitable)
+        await to_await
+        q.task_done()
+        await rate_limiter.wait()
+        to_await = await q.get()
+    q.task_done()
 
 async def perform_test(to_be_called: FastFunc, num_calls: int, expected_duration: float) -> None:
     start = time.time()
     
-    loop = asyncio.get_event_loop()
-    futures = []
-    
     async def to_call():
         await to_be_called.perform_quick_call()
 
-    for _ in range(num_calls):
-        futures.append(asyncio.ensure_future(to_call(), loop=loop))
+    for _i in range(num_calls):
+        await to_be_called.queue.put(to_call())
+    await to_be_called.queue.put(STOP_FETCHING)
 
-    await asyncio.gather(*futures)
+    results = await asyncio.gather(
+            asyncio.create_task(consume_updates(rate_limiter=to_be_called.limiter, q=to_be_called.queue)),
+            return_exceptions=True,
+        )
     duration = time.time() - start
     assert to_be_called.calls == num_calls
     # More or less 0.2 sec, sounds reasonable
     assert abs(duration - expected_duration) < 0.5, f"duration was {duration}s, but expected around {expected_duration}s"
 
-def test_LeakyBucketLimiter():
+def test_LeakyBucketLimiterWorking():
+    # For some reason this is working
+    num_calls = 6
     async def my_test() -> None:
-        to_be_called = FastFunc(LeakyBucketLimiter(300, capacity=3))
-        await perform_test(to_be_called, num_calls=300, expected_duration=1.0)
+        to_be_called = FastFunc(LeakyBucketLimiter(num_calls, capacity=1))
+        await perform_test(to_be_called, num_calls=num_calls, expected_duration=1.0)
     asyncio.run(my_test())
 
+
+def test_LeakyBucketLimiterBroken():
+    # For some reason, this statically fails
+    num_calls = 300
+    async def my_test() -> None:
+        to_be_called = FastFunc(LeakyBucketLimiter(num_calls, capacity=1))
+        await perform_test(to_be_called, num_calls=num_calls, expected_duration=1.0)
+    asyncio.run(my_test())
 
 def test_StrictLimiter():
     async def my_test() -> None:
